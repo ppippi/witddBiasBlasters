0b04a91e007a53ae7572bb5ea60c6e1a3575bc3f
Nikoleta <nvkoleva@yahoo.com>
2017-06-17 14:41:00 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component }          from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Example 1</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bias Blasting - Changing Gender Biases';
+}
